be7c4e5de625409a732a98e4ec2ef642113d4841
sforte <fortesimone90@gmail.com>
2015-06-01 22:36:47 +0200

    fixing initial dialog

--- nervousnetrel/src/ch/ethz/soms/nervous/android/MainActivity.java
+++ nervousnetrel/src/ch/ethz/soms/nervous/android/MainActivity.java
@@ -174,6 +174,7 @@ public class MainActivity extends Activity {
 						public void onClick(DialogInterface dialog, int which) {
 							dialog.cancel();
 							startStopSensorService(true);
+							refreshOnOffButtons();
 						}
 					});
 			builder.setNegativeButton(getString(R.string.no),
