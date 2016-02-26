0049946337e5abb018cbe28a25fde0f3fca26dd5
mistelip <mistelip@ethz.ch>
2016-02-26 11:19:15 +0100

    Imported to Android Studio Use nervousnetrel_AndroidStudio from now on

---
+++ nervousnetrel_AndroidStudio/nervousnetrel/src/main/java/ch/ethz/soms/nervous/android/sensorQueries/SensorQueriesPressure.java
@@ -0,0 +1,35 @@
+package ch.ethz.soms.nervous.android.sensorQueries;
+
+import java.io.File;
+
+import ch.ethz.soms.nervous.android.Queries.QueryNumSingleValue;
+import ch.ethz.soms.nervous.android.sensors.SensorDescPressure;
+import ch.ethz.soms.nervous.nervousproto.SensorUploadProtos.SensorUpload.SensorData;
+
+
+public class SensorQueriesPressure extends
+		QueryNumSingleValue<SensorDescPressure> {
+
+	@Override
+	public
+	long getSensorId() {
+		return SensorDescPressure.SENSOR_ID;
+	}
+
+	public SensorQueriesPressure(long timestamp_from, long timestamp_to,
+			File file) {
+		super(timestamp_from, timestamp_to, file);
+	}
+
+	@Override
+	public
+	SensorDescPressure createSensorDescSingleValue(SensorData sensorData) {
+		return new SensorDescPressure(sensorData);
+	}
+
+	@Override
+	public
+	SensorDescPressure createDummyObject() {
+		return new SensorDescPressure(0, 0);
+	}
+}
